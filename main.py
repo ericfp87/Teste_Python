5ba87c2247ffa7ddb7b2377402332a9321e2c0f4
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-07-23 21:24:02 -0300

    criação db

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret1234567890'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@localhost/users'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "usuarios"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+
+
+db.create_all()
+
 
 @app.route('/')
 def home():
@@ -8,10 +27,37 @@ def home():
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        pwd = User.query.filter_by(password=password).first()
+        if not user:
+            flash("Este usuário não existe. Tente novamente!")
+            return redirect(url_for('login'))
+        elif not pwd:
+            flash("Senha Inválida! Tente novamente!")
+            return redirect(url_for('login'))
+
+        else:
+            return redirect(url_for('index'))
     return render_template('login.html')
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+
+        new_user = User(username=request.form.get("username"),
+                        password=request.form.get("password")
+                        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+
     return render_template('register.html')
 
 @app.route('/index')
