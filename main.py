484e473a47a907f0f6f3f577cdaa5f881d82824f
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-07-26 17:46:33 -0300

    final

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import datetime
 import jwt
 from functools import wraps
 import random
-
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -19,13 +18,14 @@ db = SQLAlchemy(app)
 
 
 
-
+#CRIAR TABELA USUARIO
 class User(db.Model):
     __tablename__ = "usuarios"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(1000), nullable=False)
 
+#CRIAR TABELA DE APOSTAS
 class Game(db.Model):
     __tablename__ = "jogos"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -38,7 +38,7 @@ lista_aposta = []
 final = []
 token = None
 
-
+#DECORATOR DA AUTENTICAÇÃO JWT (NÃO TERMINADO)
 def jwt_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
@@ -57,25 +57,20 @@ def jwt_required(f):
             # print(token_pure)
             decoded = jwt.decode(token, app.config['SECRET_KEY'])
             print(decoded)
-            current_user = User.query.get(username=decoded['username']).first()
+            current_user = User.query.filter_by(username=decoded['username']).first()
             print(current_user)
         except:
             return jsonify({"error" : "Token invalido"}), 401
         return f(current_user, *args, **kwargs)
     return wrapper
 
-@app.shell_context_processor
-def make_shell_context():
-    return dict (
-        app=app,
-        db=db,
-        User=User
-    )
 
+#ROTA PRINCIPAL
 @app.route('/')
 def home():
     return render_template('home.html')
 
+#ROTA PARA LOGIN
 @app.route('/login', methods=["GET", "POST"])
 def login():
 
@@ -84,9 +79,7 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
 
-
         user = User.query.filter_by(username=username).first()
-
 
         if not user:
             flash("Este usuário não existe. Tente novamente!")
@@ -101,17 +94,16 @@ def login():
         }
         token = jwt.encode(payload, app.config['SECRET_KEY'])
 
-
         return redirect(url_for('index'))
     return render_template('login.html')
 
 
-
+#ROTA PARA REGISTARR USUÁRIO
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
 
-        hashed_password = generate_password_hash(request.form.get("password"), method='sha256')
+        hashed_password = generate_password_hash(request.form.get("password"))
 
         new_user = User(username=request.form.get("username"),
                         password=hashed_password
@@ -124,46 +116,58 @@ def register():
 
     return render_template('register.html')
 
+#ROTA PARA PÁGINA INDEX APÓS LOGAR
 @app.route('/index')
+# @jwt_required
 def index():
     return render_template('index.html')
 
+#ROTA PARA DESCONECTAR USUARIO (AUTENTICAÇÃO NÃO TERMINADA)
 @app.route('/logout')
+# @jwt_required
 def logout():
     return redirect(url_for('home'))
 
+#ROTA PARA EDITAR USUARIO E SENHA
 @app.route('/update', methods=["GET", "POST"])
-@jwt_required
-def update(current_user):
-
+# @jwt_required
+def update():
+    global token
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
 
-        new_username = request.form.get("new_username")
-        new_password = request.form.get("new_password")
-
         user = User.query.filter_by(username=username).first()
-        pwd = User.query.filter_by(password=password).first()
+
         if not user:
             flash("Este usuário não existe. Tente novamente!")
             return redirect(url_for('login'))
-
-        elif not pwd:
+        if not check_password_hash(user.password, password):
             flash("Senha Inválida! Tente novamente!")
             return redirect(url_for('login'))
 
-        else:
-            con = mysql.connector.connect(host="localhost", user="admin", password="admin", db="users")
-            cur = con.cursor()
-            update = cur.execute(f"UPDATE usuarios SET username = '{new_username}', password = '{new_password}' WHERE username = '{username}';")
-            cur.execute(update)
-            con.commit()
+        payload = {
+            "id": user.id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        }
+        token = jwt.encode(payload, app.config['SECRET_KEY'])
 
-            flash("Edição Realizada com Sucesso!")
+        hashed_password = generate_password_hash(request.form.get("password"))
+        new_username = request.form.get("new_username")
+        new_password = hashed_password
+
+        con = mysql.connector.connect(host="localhost", user="admin", password="admin", db="users")
+        cur = con.cursor()
+        update = cur.execute(f"UPDATE usuarios SET username = '{new_username}', password = '{new_password}' WHERE username = '{username}';")
+        cur.execute(update)
+        con.commit()
+
+        flash("Edição Realizada com Sucesso!")
     return render_template('update.html')
 
+#ROTA PARA DELETAR USUARIO
 @app.route('/delete', methods=["GET", "POST"])
+# @jwt_required
 def delete():
     if request.method == "POST":
         username = request.form.get("username")
@@ -189,11 +193,11 @@ def delete():
             return redirect(url_for('home'))
     return render_template('delete.html')
 
-
+#ROTA PARA MOSTRAR ACERTOS NA APOSTA
 @app.route('/hits')
+# @jwt_required
 def hits():
     URL = "https://www.google.com/search?q=caixa+mega+sena"
-
 
     del final[:]
     resposta = requests.get(URL, headers={
@@ -214,7 +218,7 @@ def hits():
     return render_template('hits.html', acertos=acertos)
 
 
-
+#ROTA PARA MOSTRAR ULTIMO RESULTADO DA MEGA-SENA
 @app.route('/result')
 def result():
     URL = "https://www.google.com/search?q=caixa+mega+sena"
@@ -229,8 +233,9 @@ def result():
     print(final)
     return render_template('last_mega.html', final=final)
 
+#ROTA PARA CRIAR UM NOVO JOGO
 @app.route('/new', methods=["GET", "POST"])
-
+# @jwt_required
 def new_game():
     if request.method == "POST":
 
@@ -257,7 +262,9 @@ def new_game():
 
     return render_template('new_game.html')
 
+#ROTA PARA LISTAR TODAS AS APOSTAS
 @app.route("/list")
+# @jwt_required
 def get_all_games():
     con = mysql.connector.connect(host="localhost", user="admin", password="admin", db="users")
     cur = con.cursor()
